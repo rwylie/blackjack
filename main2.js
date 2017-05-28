aaa49852e8e11dae92a538a400ddddacd1f0d54c
Ronda Wylie <rondawylie@yahoo.com>
2017-05-28 12:55:35 -0500

    bJ-refactored deck and hand working

--- main2.js
+++ main2.js
@@ -1,30 +1,58 @@
-$(document).ready(function() {
-$('#deal-button').click (function() {
-  dealACard(dealerHand, "#dealer-hand");
-  dealACard(playerHand, '#player-hand');
-  dealACard(dealerHand, "#dealer-hand");
-  dealACard(playerHand, '#player-hand');
-});
+class Hand {
+  constructor(name) {
+    this.name = name;
+    this.cards = [];
+  }
 
-$('#hit-button').click (function() {
-  dealACard(playerHand, '#player-hand');
-});
+  addCard(card) {
+    this.cards.push(card);
+  }
 
-var deckArray = []
-
-function buildDeck() {
-  var face = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace'];
-  var suit = ['clubs','spades','diamonds','hearts'];
-  face.forEach(function(face) {
-    suit.forEach(function(suit) {
-      deckArray.push({
-        'face': face,
-        'suit': suit,
-        'img': face + '_of_' + suit + '.png'})
-    });
-  });
-  return deckArray;
+  getPoints() {
+    var cardArray = this.cards;
+    var pointsTotal = 0;
+    for(var i = 0; i < cardArray.length; i++) {
+      pointsTotal += cardArray[i].points;
+    }
+    return pointsTotal;
+  }
 }
 
+class Card {
+  constructor (points, suit) {
+  this.points = points;
+  this.suit = suit;
+  }
+}
 
-});
+class Deck {
+  constructor(){
+    this.deckArray = [];
+  }
+  addDeck(newDeck) {
+    this.deckArray.push(newDeck);
+  }
+  numCardsLeft() {
+    return this.deckArray.length;
+  }
+  makeDeck() {
+    var label = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace'];
+    var suit = ['clubs','spades','diamonds','hearts'];
+    var self = this;
+    label.forEach(function(label) {
+      suit.forEach(function(suit) {
+        self.deckArray.push({'label': label, 'suit': suit, 'img':label + '_of_' + suit + '.png'})
+      });
+    });
+  }
+}
+
+var myDeck = new Deck();
+myDeck.makeDeck();
+console.log(myDeck);
+
+var myHand = new Hand();
+myHand.addCard(new Card(5, 'diamonds'));
+myHand.addCard(new Card(10, 'hearts'));
+
+myHand.getPoints();
